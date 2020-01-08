26f7b8c2bf16738414180006ddc4635f1225cebd
amine <labassiamine@gmail.com>
2020-01-08 12:50:16 +0100

    all ok

--- Stickers Mortels Adèle/PackTableViewCell.swift
+++ Stickers Mortels Adèle/PackTableViewCell.swift
@@ -13,12 +13,18 @@ class PackTableViewCell: UITableViewCell {
     @IBOutlet weak var packIconImage: UIImageView!
     @IBOutlet weak var packNameLabel: UILabel!
     @IBOutlet weak var packSizeLabel: UILabel!
+    @IBOutlet weak var plusBtn: UIImageView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
         packNameLabel.font = UIFont.preferredFont(forTextStyle: .headline)
         packSizeLabel.font = UIFont.preferredFont(forTextStyle: .caption1)
         packSizeLabel.textColor = .gray
+        if #available(iOS 13.0, *) {
+            plusBtn.image = UIImage(systemName: "plus")
+        } else {
+            plusBtn.image = UIImage(named: "addicn")
+        }
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
