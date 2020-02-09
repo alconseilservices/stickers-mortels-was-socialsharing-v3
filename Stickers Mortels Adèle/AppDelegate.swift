aa56eb3b7a62cbeeace14fc1424838c0687ac0e5
amine <labassiamine@gmail.com>
2020-02-10 00:32:14 +0100

    configure in app message and landing page

--- Stickers Mortels Adèle/AppDelegate.swift
+++ Stickers Mortels Adèle/AppDelegate.swift
@@ -14,14 +14,16 @@ import Batch
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var notificationDelegate = NotificationDelegate()
+    var inAppMsgDelegate = InAppMsgDelegate()
     var center = UNUserNotificationCenter.current()
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        // BatchPush.disableAutomaticIntegration()
+        BatchPush.disableAutomaticIntegration()
         Batch.start(withAPIKey: "DEV56E6C3FDD1D50248213DCDB910F") // dev
         // Batch.start(withAPIKey: "56E6C3FDCEAC5B0F3D6AAEA38CB6AC") // live
+        //BatchMessaging.setDelegate(inAppMsgDelegate)
         BatchPush.requestNotificationAuthorization()
         BatchPush.refreshToken()
         
@@ -31,21 +33,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
 
-    // MARK: UISceneSession Lifecycle
-    @available(iOS 13.0, *)
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
 
-    @available(iOS 13.0, *)
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-    /*
     func application(_ application: UIApplication, didRegister notificationSettings: UIUserNotificationSettings) {
         BatchPush.handleRegister(notificationSettings)
     }
@@ -67,6 +55,24 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         BatchPush.handleNotification(userInfo, actionIdentifier: identifier)
         completionHandler()
     }
- */
+ 
+}
+
+@objc
+class InAppMsgDelegate: NSObject, BatchMessagingDelegate {
+    
+    func batchMessageDidTriggerAction(_ action: BatchMessageAction, messageIdentifier identifier: String?, actionIndex index: Int) {
+        var dd = ""
+    }
+    
+    func batchInAppMessageReady(message: BatchInAppMessage) {
+        do {
+            let vc = try BatchMessaging.loadViewController(for: message)
+            BatchMessaging.present(vc)
+        } catch _ as NSError {
+            // Handle the error
+        }
+    }
+    
 }
 
